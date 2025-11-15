5849d1ece03c0039a53370a75c3bbacdcf5f25b1
Liliia Galkina <vvlili@yandex.ru>
2025-11-15 19:30:44 +0300

    изменение структуры папок

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./reset.scss";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import HomePage from "./components/Main/HomePage";
+import HomePage from "./components/Main/HomePage/HomePage";
 
 function App() {
   return (
