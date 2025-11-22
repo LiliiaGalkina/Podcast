f4f434c35836d924c6a22dd0ea92b144d77f384f
Liliia Galkina <vvlili@yandex.ru>
2025-11-22 14:27:46 +0300

    форма с валидацией

--- src/components/Main/About/MessageForm.jsx
+++ src/components/Main/About/MessageForm.jsx
@@ -1,7 +1,39 @@
 import style from "./messageform.module.scss";
 import Buttondark from "../../Button/Buttondark";
+import { useState } from "react";
 
 const MessageForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+	const [isEmailValidate, setIsEmailValidate] = useState(true);
+	const [isPhoneValidate, setIsPhoneValidate] = useState(true);
+
+
+  const handleChangeEmail = (e) => {
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    if (emailRegex.test(e.target.value)) {
+      setIsEmailValidate(true);
+    } else {
+      setEmail("");
+      setIsEmailValidate(false);
+    }
+  };
+
+	const handleChangePhone = (e) => {
+	   const phoneRegex = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
+     if (phoneRegex.test(e.target.value)) {
+		 setIsPhoneValidate(true);
+		 console.log(true)
+     } else {
+       setPhone("");
+		 setIsPhoneValidate(false);
+		 console.log(false);
+     }
+  };
+
   return (
     <form action="#" className={style.getintoushform}>
       <div className={style.fields}>
@@ -12,7 +44,10 @@ const MessageForm = () => {
           <input
             type="text"
             id="name"
-            className={style.fildinput} required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className={style.fildinput}
+            required
             placeholder="Full name"
           />
         </div>
@@ -23,9 +58,17 @@ const MessageForm = () => {
           <input
             type="email"
             id="email"
-            className={style.fildinput} required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={handleChangeEmail}
+            className={style.fildinput}
+            style={{ borderColor: !isEmailValidate ? "#cd4631" : "#000000" }}
+            required
             placeholder="johndoe@gmail.com"
           />
+          <p className={style.error}>
+            {!isEmailValidate ? "Введите корректный Email" : ""}
+          </p>
         </div>
         <div className={style.field}>
           <label htmlFor="phone" className={style.fieldlabel}>
@@ -34,9 +77,16 @@ const MessageForm = () => {
           <input
             type="tel"
             id="phone"
-            className={style.fildinput} required
-            placeholder="Phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            onBlur={handleChangePhone}
+            className={style.fildinput}
+            required
+            placeholder="+0 000-000-00-00"
           />
+          <p className={style.error}>
+            {!isPhoneValidate ? "Введите корректный номер телефона" : ""}
+          </p>
         </div>
         <div className={style.field}>
           <label htmlFor="subject" className={style.fieldlabel}>
@@ -45,7 +95,10 @@ const MessageForm = () => {
           <input
             type="text"
             id="subject"
-            className={style.fildinput} required
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            className={style.fildinput}
+            required
             placeholder="Subject..."
           />
         </div>
@@ -56,7 +109,10 @@ const MessageForm = () => {
           <textarea
             name="message"
             id="message"
-            className={style.fieldmessage} required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            className={style.fieldmessage}
+            required
             placeholder="Your message goes here..."
           ></textarea>
         </div>
@@ -67,7 +123,10 @@ const MessageForm = () => {
         </p>
         <Buttondark buttonText="SEND MESSAGE" />
         <div className={style.info}>
-          Viverra at port accumsan. <a href="#" className={style.infolink}>Orci non</a>
+          Viverra at port accumsan.{" "}
+          <a href="#" className={style.infolink}>
+            Orci non
+          </a>
         </div>
       </div>
     </form>
