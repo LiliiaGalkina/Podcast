c7790c3281a1c11f6e52fb9422c9251d5fc634a4
Liliia Galkina <vvlili@yandex.ru>
2025-12-08 20:24:45 +0300

    слияние

--- src/components/Main/About/Aboutsponsorslider.jsx
+++ src/components/Main/About/Aboutsponsorslider.jsx
@@ -1,130 +1,33 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import style from "./aboutsponsorslider.module.scss";
+import { sponsor } from "../newsitems";
 import arrowleft from "/image/homepage/sliderfeedback/arrowleft.png";
 import arrowright from "/image/homepage/sliderfeedback/arrowright.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/pagination";
+import "./swiperstyles.scss";
+import { Pagination, Navigation } from "swiper/modules";
 
 const AboutSponsorSlider = () => {
-  const SlickButtonFix = ({ currentSlide, slideCount, children, ...props }) => (
-    <div {...props}>{children}</div>
-  );
-
-  const settings = {
-    dots: false,
-    arrows: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    focusOnSelect: true,
-    nextArrow: (
-      <SlickButtonFix>
-        <div>
-          <img
-            src={arrowright}
-            className={style.arrowright}
-            alt="стрелка вправо"
-          />
-        </div>
-      </SlickButtonFix>
-    ),
-    prevArrow: (
-      <SlickButtonFix>
-        <div>
-          <img
-            src={arrowleft}
-            className={style.arrowleft}
-            alt="стрелка влево"
-          />
-        </div>
-      </SlickButtonFix>
-    ),
-    responsive: [
-      {
-        breakpoint: 1300,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
 
   return (
-    <div className={style.slidersponsorcontainer}>
-      <Slider {...settings} className={style.sliderfeedback}>
-        <div className={style.slidersponsoritem}>
-          <div className={style.itemimg}>
-            <img src="/Podcast/image/about/spotify.png" alt="spotify" />
-          </div>
-          <p className={style.itemtext}>
-            Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in
-            sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu
-            in auctor. Gravida leo et.
-          </p>
-        </div>
-        <div className={style.slidersponsoritem}>
-          <div className={style.itemimg}>
-            <img
-              src="/Podcast/image/about/googlepodcasts.png"
-              alt="googlepodcasts"
-            />
-          </div>
-          <p className={style.itemtext}>
-            Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in
-            sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu
-            in auctor. Gravida leo et.
-          </p>
-        </div>
-
-        <div className={style.slidersponsoritem}>
-          <div className={style.itemimg}>
-            <img src="/Podcast/image/about/youtube.png" alt="youtube" />
-          </div>
-          <p className={style.itemtext}>
-            Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in
-            sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu
-            in auctor. Gravida leo et.
-          </p>
-        </div>
-        <div className={style.slidersponsoritem}>
-          <div className={style.itemimg}>
-            <img
-              src="/Podcast/image/about/googlepodcasts.png"
-              alt="googlepodcasts"
-            />
-          </div>
-          <p className={style.itemtext}>
-            Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in
-            sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu
-            in auctor. Gravida leo et.
-          </p>
-        </div>
-        <div className={style.slidersponsoritem}>
-          <div className={style.itemimg}>
-            <img src="/Podcast/image/about/spotify.png" alt="spotify" />
-          </div>
-          <p className={style.itemtext}>
-            Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in
-            sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu
-            in auctor. Gravida leo et.
-          </p>
-        </div>
-        <div className={style.slidersponsoritem}>
-          <div className={style.itemimg}>
-            <img src="/Podcast/image/about/youtube.png" alt="youtube" />
-          </div>
-          <p className={style.itemtext}>
-            Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in
-            sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu
-            in auctor. Gravida leo et.
-          </p>
-        </div>
-      </Slider>
+    <div className={style.slider}>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={20}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {sponsor.map((item) => (
+          <SwiperSlide className={style.slidersponsoritem} key={item.id}>
+            <div className={style.itemimg}>
+              <img src={item.imagesrc} alt={item.imgalt} />
+            </div>
+            <p className={style.itemtext}>{item.text}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
--- src/components/Main/newsitems.js
+++ src/components/Main/newsitems.js
@@ -319,3 +319,4 @@ export const sponsor = [
     text: "Quis dictum cursus faucibus mattis dignissim. Pellentes que purus in sed sodales in mauris molestie. Eleifend estcon sctetur interdum eu in auctor. Gravida leo et.",
   },
 ];
+
