547bd0e15002e0101abdb0e5e96263412799b4e5
Liliia Galkina <vvlili@yandex.ru>
2025-12-08 20:44:32 +0300

    about slider responsive

--- src/components/Main/About/Aboutsponsorslider.jsx
+++ src/components/Main/About/Aboutsponsorslider.jsx
@@ -1,20 +1,21 @@
 import style from "./aboutsponsorslider.module.scss";
 import { sponsor } from "../newsitems";
-import arrowleft from "/image/homepage/sliderfeedback/arrowleft.png";
-import arrowright from "/image/homepage/sliderfeedback/arrowright.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/pagination";
 import "./swiperstyles.scss";
-import { Pagination, Navigation } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+import useResponsiveEvent from "./useResponsiveEvent.jsx";
 
 const AboutSponsorSlider = () => {
-
+	const isMobileScreen = useResponsiveEvent(769);
+	const isMobileScreenLittle = useResponsiveEvent(481);
+	
   return (
     <div className={style.slider}>
       <Swiper
-        slidesPerView={3}
+        slidesPerView={isMobileScreenLittle ? 1 : isMobileScreen ? 2 : 3}
         spaceBetween={20}
         navigation={true}
         modules={[Navigation]}
